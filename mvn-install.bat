4b2a031798d3b33ef061c320226b09814da0ca49
Andy Dennie <github@adennie.e4ward.com>
2013-01-10 14:55:04 -0500

    updated install batch file to use com.fizzbuzz for maven groupId

--- mvn-install.bat
+++ mvn-install.bat
@@ -3,7 +3,7 @@ set version=%1%
 if (%version%)==() goto MISSING_VERSION
 
 copy sdk\bin\sdk.jar sdk\bin\socialize-sdk-android-%version%.jar
-mvn install:install-file -Dpackaging=jar -DgeneratePom=true -DgroupId=com.socialize -DartifactId=socialize-sdk-android -Dversion=%version% -Dfile=sdk\bin\socialize-sdk-android-%version%.jar
+mvn install:install-file -Dpackaging=jar -DgeneratePom=true -DgroupId=com.fizzbuzz -DartifactId=socialize-sdk-android -Dversion=%version% -Dfile=sdk\bin\socialize-sdk-android-%version%.jar
 
 goto END
 
